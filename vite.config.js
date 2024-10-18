d13c17418cc7a23f5c03ea95d02619d77a40db30
Stogniew <stogniew.a@northeastern.edu>
2024-10-18 11:45:15 -0400

    ballot icon

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/whos-on-your-ballot/',
+  base: '/whos-on-your-ballot/',
   plugins: [svelte()],
 })
